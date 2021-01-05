ce3e4660471b83dbc0dc6e4cfcaf0b00a4a97cbe
Gideon Moore <gmoore016@gmail.com>
2021-01-05 12:06:53 -0500

    Added argument parsing

---
+++ designerbot.py
@@ -0,0 +1,21 @@
+import atoma
+import requests
+import argparse
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="Description: {}".format(__file__))
+
+    arg_parser.add_argument(
+        '-s', "--source", action="store", required=True, help="The source feed to tweet"
+    )
+
+    arg_parser.add_argument(
+        '-f', "--frequency", action="store", default="d", choices=['m', 'h', 'd', 'w'], help="The timeframe to check for posts. Can be 'm', 'h', 'd', or 'w' for minute, hour, day, and week respectively."
+    )
+
+    args = arg_parser.parse_args()
+
+
+
+
+main()
