af77c20a9cdff113a47671fc7a29693544addaf1
Gideon Moore <gmoore016@gmail.com>
2021-01-05 14:50:17 -0500

    Finished Twitter API work

--- TwitteRSS.py
+++ TwitteRSS.py
@@ -3,8 +3,7 @@ import requests
 import argparse
 import datetime
 import tweepy
-
-TIMESTAMP_REGEX = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}"
+import os
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Description: {}".format(__file__))
@@ -21,8 +20,14 @@ def main():
 
     new_posts = get_new_posts(**vars(args))
 
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+
+    access_token = os.environ['ACCESS_TOKEN']
+    access_secret = os.environ['ACCESS_SECRET']
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_token, access_secret)
 
     api = tweepy.API(auth)
 
