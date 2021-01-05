b2f127c667c3045813c010ee0c2fe2bfcde0c2df
Gideon Moore <gmoore016@gmail.com>
2021-01-05 12:37:47 -0500

    Finished timestamp threshold function

--- designerbot.py
+++ designerbot.py
@@ -1,6 +1,7 @@
 import atoma
 import requests
 import argparse
+import datetime
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Description: {}".format(__file__))
@@ -15,7 +16,68 @@ def main():
 
     args = arg_parser.parse_args()
 
+    new_posts = get_new_posts(**vars(args))
 
+    for post in new_posts:
+        tweet(post)
+
+
+def get_new_posts(source, frequency):
+    # Pull feed from web
+    response = requests.get(source)
+    feed = atoma.parse_atom_bytes(response.content)
+
+    # Calculate standard for "new" posts
+    threshold = get_threshold_time(frequency)
+
+    # Array to store new posts
+    new_posts = []
+
+    # Interate through entries starting from newest
+    for entry in feed.entries[0:4]:
+        # Convert string timestamp to Unix time
+        timestamp_string = entry.updated
+        published_time = calculate_unix_time(timestamp_string)
+
+        # If the entry is new, add it to list to publish
+        if published_time > threshold:
+            new_posts.append(entry)
+
+        # Otherwise, we've reached the end of the new entries
+        # and are done
+        else:
+            break
+
+    # Return all posts published after threshold
+    return new_posts
+
+
+
+
+def get_threshold_time(frequency):
+    """Function to calculate threshold time. Articles published
+    after threshold time are "new" and should be processed. """
+    # Pulls current time to determine "newness"
+    now = datetime.datetime.now()
+
+    # Determines time frequency
+    if frequency == 'm':
+        delta = 1
+    elif frequency == 'h':
+        delta = 60
+    elif frequency == 'd':
+        delta = 24 * 60
+    elif frequency == 'w':
+        delta = 24 * 60 * 7
+    else:
+        raise Exception
+
+    # If posts occurred after this time, they're new
+    threshold = now - datetime.timedelta(minutes=delta)
+    return threshold
+
+def tweet(post):
+    print(post)
 
 
 main()
